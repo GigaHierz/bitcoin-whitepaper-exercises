d40471b2acc21cd105e474bb1f4f5df399bff249
GigaHierz <lenahierzi@hotmail.de>
2021-09-16 01:21:06 +0200

    Solve hashing excersise

--- hashing/hashing.js
+++ hashing/hashing.js
@@ -4,7 +4,7 @@ var crypto = require("crypto");
 
 // The Power of a Smile
 // by Tupac Shakur
-var poem = [
+const poem = [
 	"The power of a gun can kill",
 	"and the power of fire can burn",
 	"the power of wind can chill",
@@ -15,7 +15,7 @@ var poem = [
 	"especially yours can heal a frozen heart",
 ];
 
-var Blockchain = {
+let Blockchain = {
 	blocks: [],
 };
 
@@ -27,17 +27,53 @@ Blockchain.blocks.push({
 	timestamp: Date.now(),
 });
 
-// TODO: insert each line into blockchain
-// for (let line of poem) {
-// }
 
-// console.log(`Blockchain is valid: ${verifyChain(Blockchain)}`);
+
+
+// TODO: insert each line into blockchain
+function createBlock(poems) {
+	let i = 0;
+
+	poems.map((line) => {
+		let prevHash = Blockchain.blocks[i].hash
+		Blockchain.blocks.push({
+			prevHash,
+			index: ++i,
+			hash: "",
+			data: line,
+			timestamp: Date.now(),
+		});
+		Blockchain.blocks[i].hash = blockHash(
+			Blockchain.blocks[i]
+		);
+	});
+
+};
+
+// remove the first line of the poem and add it to the genisis block.
+Blockchain.blocks[0].data = poem.shift(1);
+createBlock(poem);
+
+
+console.log(`Blockchain is valid: ${verifyChain(Blockchain)}`);
+
+function verifyChain(blockchain) {
+	return blockchain.blocks.map((block) => verifyBlock(block)).filter((el) => el === false).length === 0;
+}
+
+
+function verifyBlock(bl) {
+	return bl.index === 0 ?
+		bl.hash === "000000"
+		: bl.data !== '' && bl.prevHash === Blockchain.blocks[bl.index - 1].hash && bl.index >= 0 && bl.hash === blockHash(bl);
+}
 
 
 // **********************************
 
 function blockHash(bl) {
 	return crypto.createHash("sha256").update(
+		bl.prevHash + bl.index + bl.data + bl.timestamp
 		// TODO: use block data to calculate hash
 	).digest("hex");
 }
